150aff1411a69308237fb7b14f71310b13802f47
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-04-25 14:58:03 -0500

    Adding base Entity class.

---
+++ Gbm.Eshop/Gbm.Ordering.Domain/Common/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Gbm.Ordering.Domain.Common
+{
+    public abstract class Entity
+    {
+        int? requestedHashCode;
+        int id;
+
+        public virtual int Id
+        {
+            get
+            {
+                return id;
+            }
+            protected set
+            {
+                id = value;
+            }
+        }
+
+        
+    }
+}
