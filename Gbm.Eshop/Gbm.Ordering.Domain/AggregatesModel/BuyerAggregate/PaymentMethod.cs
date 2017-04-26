d2e599ff5563d9aecf905667bdbc0e502cdc166c
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-04-26 15:02:15 -0500

    Adding paymentMethod class

--- Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/BuyerAggregate/PaymentMethod.cs
+++ Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/BuyerAggregate/PaymentMethod.cs
@@ -8,6 +8,20 @@ namespace Gbm.Ordering.Domain.AggregatesModel.BuyerAggregate
 {
     public class PaymentMethod : Entity
     {
-        
+        private readonly string alias;
+        private readonly string cardNumber;
+        private readonly string securityNumber;
+        private readonly string cardHolderName;
+        private readonly DateTime expiration;
+
+        public CardType CardType { get; private set; }
+
+        protected PaymentMethod() { }
+
+        public PaymentMethod(CardType cardType, string alias, string cardNumber, 
+            string securityNumber, string cardHolderName, DateTime expiration)
+        {
+
+        }
     }
 }
