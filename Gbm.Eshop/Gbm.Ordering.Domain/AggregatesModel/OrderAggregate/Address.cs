97171779ace988302aaf4ba2442c05a469c95860
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-04-27 11:51:05 -0500

    Adding OrderAggregate classes

---
+++ Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/OrderAggregate/Address.cs
@@ -0,0 +1,38 @@
+﻿using Gbm.Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gbm.Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; private set; }
+
+        public string City { get; private set; }
+
+        public string State { get; private set;  }
+
+        public string Country { get; private set; }
+
+        public string ZipCode { get; private set; }
+
+        public Address(string street, string city, string state, string country, string zipCode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipCode;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+        }
+    }
+}
---
+++ Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/OrderAggregate/OrderItem.cs
@@ -0,0 +1,60 @@
+﻿using Gbm.Ordering.Domain.Common;
+using Gbm.Ordering.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gbm.Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        private string productName;
+        private Uri pictureUri;
+        private decimal unitPrice;
+        private decimal discount;
+        private int units;
+
+        public int ProductId { get; set; }
+
+        public OrderItem(int productId, string productName, 
+            decimal unitPrice, decimal discount, int units, Uri pictureUri)
+        {
+            if(units <= 0)
+            {
+                throw new OrderDomainException("Invalid number of units");
+            }
+
+            // TODO: Apply Extract Class Refactoring: to move this rules
+            if((unitPrice * units) < discount)
+            {
+                throw new OrderDomainException("The total of the order item is lower than the applied discount");
+            }
+
+            this.ProductId = productId;
+            this.productName = productName;
+            this.unitPrice = unitPrice;
+            this.discount = discount;
+            this.units = units;
+            this.pictureUri = pictureUri;
+        }
+
+        public void SetPictureUri(Uri pictureUri)
+        {
+            if (pictureUri != null)
+            {
+                this.pictureUri = pictureUri;
+            }
+        }
+
+        public void SetNewDiscount(decimal discount)
+        {
+            if(discount <= 0)
+            {
+                throw new OrderDomainException("Discount amount is invalid");
+            }
+
+            this.discount = discount;
+        }
+
+    }
+}
