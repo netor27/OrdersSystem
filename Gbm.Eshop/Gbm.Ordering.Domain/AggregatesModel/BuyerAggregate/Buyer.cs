f8ec761519b821b474e523a7c7e6b4d2944d18ba
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-04-27 10:16:59 -0500

    Adding Buyer Class and Updating PaymentMethod class

---
+++ Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gbm.Ordering.Domain.AggregatesModel.BuyerAggregate
+{
+    public class Buyer
+    {
+    }
+}
--- Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/BuyerAggregate/PaymentMethod.cs
+++ Gbm.Eshop/Gbm.Ordering.Domain/AggregatesModel/BuyerAggregate/PaymentMethod.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 using Gbm.Ordering.Domain.Common;
-
+using Gbm.Ordering.Domain.Exceptions;
 
 namespace Gbm.Ordering.Domain.AggregatesModel.BuyerAggregate
 {
@@ -21,7 +21,27 @@ namespace Gbm.Ordering.Domain.AggregatesModel.BuyerAggregate
         public PaymentMethod(CardType cardType, string alias, string cardNumber, 
             string securityNumber, string cardHolderName, DateTime expiration)
         {
+            this.cardNumber = !string.IsNullOrWhiteSpace(cardNumber) ? cardNumber : 
+                throw new OrderDomainException(nameof(cardNumber));
+            this.securityNumber = !string.IsNullOrWhiteSpace(securityNumber) ? securityNumber :
+                throw new OrderDomainException(nameof(securityNumber));
+            this.cardHolderName = !string.IsNullOrWhiteSpace(cardHolderName) ? cardHolderName :
+                throw new OrderDomainException(nameof(cardHolderName));
 
+            if(expiration <= DateTime.Now)
+            {
+                throw new OrderDomainException(nameof(expiration));
+            }
+
+            this.alias = alias;
+            this.expiration = expiration;
+            this.CardType = cardType;
+        }
+
+        public bool IsEqual(CardType cardType, string cardNumber, DateTime expiration)
+        {
+            return this.CardType.Id == cardType.Id && this.cardNumber == cardNumber 
+                && this.expiration == expiration;
         }
     }
 }
