3697705ad5554b968ac523a8748d47e9256e49a9
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-04-28 11:32:17 -0500

    Adding Buyer and Order repository implementations

---
+++ Gbm.Eshop/Gbm.Ordering.Infrastructure/Repositories/BuyerRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Gbm.Ordering.Domain.Common;
+using Gbm.Ordering.Domain.AggregatesModel.BuyerAggregate;
+
+namespace Gbm.Ordering.Infrastructure.Repositories
+{
+    public class BuyerRepository : IBuyerRepository
+    {
+        private readonly OrderingContext context;
+
+        public IUnitOfWork UnitOfWork { get { return context; } }
+
+        public BuyerRepository(OrderingContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Buyer Add(Buyer buyer)
+        {
+            if (buyer.IsTransient())
+            {
+                return context.Buyers.Add(buyer).Entity;
+            }
+            else
+            {
+                return buyer;
+            }
+        }
+
+        public Buyer Update(Buyer buyer)
+        {
+            return context.Buyers.Update(buyer).Entity;
+        }
+    }
+}
---
+++ Gbm.Eshop/Gbm.Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Gbm.Ordering.Domain.AggregatesModel.OrderAggregate;
+using Gbm.Ordering.Domain.Common;
+
+namespace Gbm.Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderingContext context;
+
+        public IUnitOfWork UnitOfWork { get { return context; } }
+
+        public OrderRepository(OrderingContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Order Add(Order order)
+        {
+            if (order.IsTransient())
+            {
+                return context.Orders.Add(order).Entity;
+            }
+            else
+            {
+                return order;
+            }
+        }
+
+        public Order Update(Order order)
+        {
+            return context.Orders.Update(order).Entity;
+        }
+    }
+}
